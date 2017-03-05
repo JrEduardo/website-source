62e93339c979a27205fba12cbf3802b6c0ae533d
Eduardo Junior <edujrrib@gmail.com>
2017-03-05 16:54:56 -0300

    Force clean website before automatic update

--- _deploy.sh
+++ _deploy.sh
@@ -5,6 +5,7 @@ git config --global user.name "Travis boot to build EERJ website"
 
 git clone https://${GIT_KEY}@github.com/JrEduardo/jreduardo.github.io.git dir-output
 cd dir-output
+rm -r ./*
 cp -r ../public/* ./
 git add --all *
 git commit -m "Automatic Update (build number ${TRAVIS_BUILD_NUMBER})" || true
