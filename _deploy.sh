f2fdc4b29871d9094e319e4936cf0af73bcd7b43
Eduardo Junior <edujrrib@gmail.com>
2017-03-05 09:34:23 -0300

    Install Hugo and fix the repository output name in travis

--- _deploy.sh
+++ _deploy.sh
@@ -3,7 +3,7 @@
 git config --global user.email "edujrrib@gmail.com"
 git config --global user.name "Travis boot to build EERJ website"
 
-git clone https://${GIT_KEY}@github.com/jreduardo.github.io.git dir-output
+git clone https://${GIT_KEY}@github.com/JrEduardo/jreduardo.github.io.git dir-output
 cd dir-output
 cp -r ../public/* ./
 git add --all *
